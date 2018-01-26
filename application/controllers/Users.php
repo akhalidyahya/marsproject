62ce0b3f2e9add8d672c88d2cfeb9550395ce4fc
RULNJT0PZN1BSFU\Administrator <khalid.yahya9@gmail.com>
2018-01-27 03:47:09 +0700

    Revert "user"
    This reverts commit 95aa8e6ecaba464dc14fa717c46671197682dbb7.

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,40 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users extends CI_Controller {
 
-	function __construct()
-	{
-		parent::__construct();
-		$this->load->model('mod_user');
-	}
-
 	public function index()
 	{
-		$data = array(
-			'data_admin' => $this->mod_user->getData()->result()
-		);
 		$this->load->view('layout/header');
 		$this->load->view('layout/sidebar');
-		$this->load->view('admin/users',$data);
+		$this->load->view('admin/users');
 		$this->load->view('layout/footer');
-	}
-
-	public function adduser(){
-		$data = array(
-			'title' => 'Form tambah user'
-		);
-		$this->load->view('layout/header');
-		$this->load->view('layout/sidebar');
-		$this->load->view('admin/form/form_users',$data);
-		$this->load->view('layout/footer');
-	}
-
-	public function deleteUser($id){
-		$this->mod_user->delete($id);
-		redirect('users');
-	}
-
-	public function deleteAllUser(){
-		$this->mod_user->deleteAll();
-		redirect('users');
 	}
 }
